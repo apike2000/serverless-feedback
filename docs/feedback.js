332c0f7bf82b57e669401c14479275911bb7fed0
Andrew <andrew_pike@nl.ibm.com@Andrews-MacBook-Pro.local>
2020-01-31 10:39:39 +0100

    Update apiurl

--- docs/feedback.js
+++ docs/feedback.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://d526c02f.eu-gb.apiconnect.appdomain.cloud/07-feedback';
+const apiUrl = 'https://7a382bc7.eu-gb.apigw.appdomain.cloud/15-feedback';
 const feedback = {
   // retrieve the existing feedback entries
   get() {
